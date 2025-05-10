c50598bdb43b27e7022c43884596bb30813b2319
Saulo Lucena <saulolucena12@gmail.com>
2025-05-10 20:58:38 -0300

    correcoes
    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AnimationProvider } from "./context/AnimationContext.jsx";
 import "./index.css"
+import './styles/animations.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
